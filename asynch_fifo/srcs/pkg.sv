1ba176ebaf9b61103725463f9629e33dbbe5f58c
vijay <vijaykrishnanunni11@gmail.com>
2026-07-10 11:56:37 +0530

    -1

---
+++ asynch_fifo/srcs/pkg.sv
@@ -0,0 +1,15 @@
+package fifo_pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  `include "transaction.sv"
+  `include "sequence.sv"
+  `include "sequencer.sv"
+  `include "driver.sv"
+  `include "monitor.sv"
+  `include "cover.sv"
+  `include "scorebaord.sv"
+  `include "agent.sv"
+  `include "env.sv"
+  `include "test.sv"
+endpackage
